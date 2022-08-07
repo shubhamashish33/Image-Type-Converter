7f7f80f300a42ecf12c6f1dd2f4a12c5c856f891
Shubham Ashish <shubhamashish8@gmail.com>
2022-08-07 18:24:39 +0530

    first commit

---
+++ imagetypeconverter.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import glob
+
+print(glob.glob("*.png"))
+
+for file in glob.glob("*.png"):
+    im = Image.open(file)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(file.replace("png", "jpg"), quality=95)
